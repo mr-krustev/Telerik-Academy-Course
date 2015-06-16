706e94292e3cea0143705c3ade3162600d5944cc
Martin Krustev <mr.krustev1994@gmail.com>
2015-06-16 14:55:05 +0300

    Add Functions and Function expressions homework;

---
+++ JavaScript OOP/Functions and Function Expressions/tasks/task-1.js
@@ -0,0 +1,30 @@
+/* Task Description */
+/* 
+ Write a function that sums an array of numbers:
+ numbers must be always of type Number
+ returns `null` if the array is empty
+ throws Error if the parameter is not passed (undefined)
+ throws if any of the elements is not convertible to Number
+
+ */
+
+function sum(numbers) {
+	var i, result, len, temp;
+	if (numbers.length === 'undefined') {
+		throw new Error();
+	}
+
+	if (numbers.length == 0) {
+		return null;
+	}
+
+	for (i = 0, len = numbers.length; i < len; i += 1) {
+		temp = +numbers[i];
+		if (isNaN(temp)) {
+			throw new Error();
+		}
+	}
+
+}
+
+module.exports = sum;
