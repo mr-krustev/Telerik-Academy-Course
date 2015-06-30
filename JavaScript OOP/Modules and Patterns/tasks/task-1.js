6c32b718969130249ade4c2a612b4a06898bbca9
Martin Krustev <mr.krustev1994@gmail.com>
2015-07-01 00:20:35 +0300

    Add needed files for Modules and Patterns homework.

---
+++ JavaScript OOP/Modules and Patterns/tasks/task-1.js
@@ -0,0 +1,162 @@
+/* Task Description */
+/* 
+ * Create a module for a Telerik Academy course
+
+ * The course has a title and presentations
+ * Each presentation also has a title
+ * There is a homework for each presentation
+
+ * There is a set of students listed for the course
+ * Each student has firstname, lastname and an ID
+ * IDs must be unique integer numbers which are at least 1
+ * Each student can submit a homework for each presentation in the course
+
+ * Create method init
+ * Accepts a string - course title
+ * Accepts an array of strings - presentation titles
+ * Throws if there is an invalid title
+ * Titles do not start or end with spaces
+ * Titles do not have consecutive spaces
+ * Titles have at least one character
+ * Throws if there are no presentations
+
+ * Create method addStudent which lists a student for the course
+ * Accepts a string in the format 'Firstname Lastname'
+ * Throws if any of the names are not valid
+ * Names start with an upper case letter
+ * All other symbols in the name (if any) are lowercase letters
+ * Generates a unique student ID and returns it
+
+ * Create method getAllStudents that returns an array of students in the format:
+ * {firstname: 'string', lastname: 'string', id: StudentID}
+
+ * Create method submitHomework
+ * Accepts studentID and homeworkID
+ * homeworkID 1 is for the first presentation
+ * homeworkID 2 is for the second one
+ * ...
+ * Throws if any of the IDs are invalid
+
+ * Create method pushExamResults
+ * Accepts an array of items in the format {StudentID: ..., Score: ...}
+ * StudentIDs which are not listed get 0 points
+ * Throw if there is an invalid StudentID
+ * Throw if same StudentID is given more than once ( he tried to cheat (: )
+ * Throw if Score is not a number
+
+ * Create method getTopStudents which returns an array of the top 10 performing students
+ * Array must be sorted from best to worst
+ * If there are less than 10, return them all
+ * The final score that is used to calculate the top performing students is done as follows:
+ * 75% of the exam result
+ * 25% the submitted homework (count of submitted homeworks / count of all homeworks) for the course
+ */
+
+function solve() {
+    var lastId = 0;
+    // private methods
+    function getNextId(){
+        return ++lastId;
+    }
+
+    var Course = {
+        // TODO: Add properties
+        get title() {
+            return this._title;
+        },
+        set title(value) {
+            var regExp = new RegExp('\\w[ ]{2,}\\w', 'g');
+            if (typeof value !== 'string') {
+                throw new Error('Title must be string!');
+            }
+
+            if (value[0] === ' ' || value[value.length - 1] === ' ') {
+                throw new Error('Title cannot start or end with a space!');
+            }
+
+            if (regExp.test(value)) {
+                throw new Error('There shouldn\'t be any consecutive spaces in the Course title!');
+            }
+
+            this._title = value;
+        },
+        get presentations() {
+            return this._presentations;
+        },
+        set presentations(value) {
+            var i, len,
+                regExp = new RegExp('\\w[ ]{2,}\\w', 'g');
+
+            if (!(Array.isArray(value))) {
+                throw new Error('Invalid presentations input.');
+            }
+
+            if (value.length == 0) {
+                throw new Error('Empty array of presentations!');
+            }
+
+            for (i = 0, len = value.length; i < len; i += 1) {
+                if (regExp.test(value[i])) {
+                    throw new Error('There shouldn\'t be any consecutive spaces in the presentations\' titles!');
+                }
+
+                if (!value[i]) {
+                    throw new Error('Presentations\' titles must not be empty!');
+                }
+            }
+            this._presentations = value;
+        },
+
+        //Constructor
+        init: function (title, presentations) {
+            this.title = title;
+            this.presentations = presentations;
+            this.students = [];
+
+            return this;
+        },
+
+        //Methods
+        addStudent: function (name) {
+            var i,len,
+                regExp = new RegExp('[^A-Za-z]'),
+                student = {},
+                names = name.split(' ');
+            //Validate!
+            if(typeof name !== 'string'){
+                throw new Error('Name must be string!');
+            }
+
+            if(names.length != 2){
+                throw new Error('Must provide First and Last name');
+            }
+
+            for(i = 0, len = names.length; i<len; i+=1){
+                if(names[i][0] !== names[i][0].toUpperCase()){
+                    throw new Error('Invalid name! First letters must be capitalized!');
+                }
+
+                if(regExp.test(names[i])){
+                    throw new Error('Invalid name! Must contain only letters!');
+                }
+            }
+
+            student.firstname = names[0];
+            student.lastname = names[1];
+            student.id = getNextId();
+            this.students.push(student)
+        },
+        getAllStudents: function () {
+        },
+        submitHomework: function (studentID, homeworkID) {
+        },
+        pushExamResults: function (results) {
+        },
+        getTopStudents: function () {
+        }
+    };
+    return Course;
+}
+
+
+module.exports = solve;
