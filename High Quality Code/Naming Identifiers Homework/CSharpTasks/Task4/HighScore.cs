ae2a6b0023f3f4e8edb984acd7cf67a39b4a4e6b
Martin Krustev <mr.krustev1994@gmail.com>
2015-07-02 14:43:57 +0300

    Move class HighScore in a separate file. Rename variables  and fix constructors and properties.

---
+++ High Quality Code/Naming Identifiers Homework/CSharpTasks/Task4/HighScore.cs
@@ -0,0 +1,30 @@
+﻿namespace MineSweeper
+{
+    public class HighScore
+    {
+        private string name;
+        private int score;
+
+        public HighScore()
+        {
+        }
+
+        public HighScore(string name, int score)
+        {
+            this.Name = name;
+            this.Score = score;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            private set { this.name = value; }
+        }
+
+        public int Score
+        {
+            get { return this.score; }
+            private set { this.score = value; }
+        }
+    }
+}
